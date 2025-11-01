5b17ecfe8f2e2532ca10725c898cb1b78541b7c3
Keerat <keeratsingh.khurana@gmail.com>
2025-11-01 10:05:01 +0100

    Updated requirements

--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load variables from .env file
 
+
 class Config:
     # Model paths
     MODEL_PATH = os.getenv("MODEL_PATH", "models/gold_lstm.h5")
@@ -14,5 +15,6 @@ class Config:
 
     # Other settings
     DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
+
 
 config = Config()
